15068a51d9b21164b10dd85c8ba43f2b2f010f3f
Jorge_Donoso <jadonosouc1977@gmail.com>
2024-08-30 20:47:47 -0400

    Update DS1.py

--- DS1.py
+++ DS1.py
@@ -8,7 +8,7 @@ tickerSymbol = 'GOOGL'
 
 tickerData = yf.Ticker(tickerSymbol)
 
-tickerDf = tickerData.history(period='id',start ='2010-5-31', end = '2020-5-31')
+tickerDf = tickerData.history(period='id',start ='2022-5-31', end = '2023-5-31')
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
