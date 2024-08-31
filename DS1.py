d6909f19ccb25802a13fa1c9f270f89f5af2d472
Jorge_Donoso <jadonosouc1977@gmail.com>
2024-08-30 21:00:43 -0400

    Update DS1.py

--- DS1.py
+++ DS1.py
@@ -8,7 +8,7 @@ tickerSymbol = 'AAPL'
 
 tickerData = yf.Ticker(tickerSymbol)
 
-tickerDf = tickerData.history(period='id',start ='2022-5-31', end = '2023-5-31')
+tickerDf = tickerData.history(period='1d',start ='2022-5-31', end = '2023-5-31')
 
 st.line_chart(tickerDf.Close)
 
