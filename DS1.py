b87b9d8f55ac15ac89dd10689e2ab863e8f454bd
Jorge_Donoso <jadonosouc1977@gmail.com>
2024-08-30 20:37:41 -0400

    Update DS1.py

--- DS1.py
+++ DS1.py
@@ -5,3 +5,11 @@ import pandas as pd
 st.write('Simple Stock Price App')
 
 tickerSymbol = 'GOOGL'
+
+tickerData = yf.ficker(tickerSymbol)
+
+tickerDf = tickerData.history(period='id',start ='2010-5-31', end = '2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+
